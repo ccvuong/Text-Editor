a7d970feba990364ffd102b1d1d65d44118784ed
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-05-09 19:50:58 -0700

    updated database code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ jate: content });
+  const result = await request;
+  console.log(result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (error) => {
+  // connect to database we want
+  const jateDb = await openDB('jate', 1);
+  // database transaction and data rights
+  const tx = jateDb.transaction('jate', 'readonly');
+  // opens specified object store
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+
+  // result?.value because result = object
+  return result?.value;
+}
 
 initdb();
